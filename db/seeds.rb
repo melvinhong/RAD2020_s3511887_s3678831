2721a4ab8e74b7792583e9d944985ff5546e186e
s3511887 <s3511887@student.rmit.edu.au>
2020-05-28 06:20:00 +1000

    create dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,36 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Post.destroy_all
-# Welcome = Post.create!(title:"Welcome To News+", content:"The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.",
-#   user_id: 6, topic_id: 5)
 
-Topic.destroy_all
-RMIT = Topic.create!(name:"RMIT")
-Game = Topic.create!(name:"Game")
-VR = Topic.create!(name:"V.R.")
-AI = Topic.create!(name:"A.I.")
 
+# Destroy all existing users
 User.destroy_all
 
-# User.create!( name:"Example User",
-#               email:"example@rails.com",
-#               mobile:"0404040404",
-#               password:"pwpwpwpw",
-#               city:"Barcelona")
-#
-# 99.times do |n|
-#   name = Faker::Name.name
-#   email = "example-#{n+1}@rails.com"
-#   password = "pwpwpwpw"
-#   Users.create!(  name:name,
-#                   email:email,
-#                   mobile:mobile,
-#                   password:password,
-#                   city:city)
+# Create main users
+User.create!(name:"RAD",email:"RAD@RAD.com",mobile:"0412345678",password:"Rails2020",city:"Melbourne")
+User.create!(name:"Melvin H", email:"mel@mel.com", mobile:"0423900100",password:"melvinrails", city:"Malaysia")
+User.create!(name:"LJ Wang", email:"lew@lew.com", mobile:"0409080706", password:"junwangrails", city:"Korea")
 
-RAD  = User.create!(name:"RAD",email:"RAD@RAD.com",mobile:"0412345678",password:"Rails2020",city:"Melbourne")
-Melvin = User.create!(name:"Melvin H", email:"mel@mel.com", mobile:"0423900100",password:"melvinrails", city:"Malaysia")
-Lew = User.create!(name:"LJ Wang", email:"lew@lew.com", mobile:"0409080706", password:"junwangrails", city:"Korea")
-SH = User.create!(name:"So Hai", email:"sohai@sohai.com", mobile:"0411223344", password:"sohairails", city:"America")
-Mahai = User.create!(name:"Ma Hai", email:"mahai@mahai.com", mobile:"0411225544", password:"mahairails", city:"Canada")
+# Generate dummy users
+9.times do |n|
+  name      = Faker::Name.name
+  email     = "example-#{n+1}@example.com"
+  mobile    = "0411223344"
+  password  = "rails2020"
+
+  User.create!(  name:      name,
+                 email:     email,
+                 mobile:    mobile,
+                 password:  password)
+end
+
+# Destroy existing topics
+Topic.destroy_all
+
+# Create main topics
+Topic.create!(name:"RMIT")
+Topic.create!(name:"Game")
+Topic.create!(name:"V.R.")
+Topic.create!(name:"A.I.")
+
+# Destroy existing posts
+Post.destroy_all
+
+# Create welcome post
+Post.create!( title:  "Hello World",
+              content:  "Welcome to News+",
+              user_id:  1,
+              topic_id: 1,)
